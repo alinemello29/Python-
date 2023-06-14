6843ce020c2beb380e3c0e88bba8353572628e43
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-06-14 09:15:15 -0400

    Add files via upload

---
+++ index10.py
@@ -0,0 +1,14 @@
+sales_w1 = [7,3,42,19,15,35,9]
+sales_w2 = [12,4,26,10,7,28]
+sales = []
+new_day = input('Enter #of lemonades for new day: ')
+sales_w2.append(int(new_day))
+#sales.extend(sales_w1)
+#sales.extend(sales_w2)
+sales = sales_w1 + sales_w2
+#sales.sort()
+worst_day_prof = min(sales) * 1.5
+best_day_prof = max(sales) * 1.5
+print(f'Worst day profit:$ {worst_day_prof}')
+print(f'Best day profit:$ {best_day_prof}')
+print(f'Combined profit:$ {worst_day_prof + best_day_prof}')
---
+++ index11.py
@@ -0,0 +1,7 @@
+msg ='Welcome  to  Python  101: Split  and Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric','John','Michael','Terry','Graham']
+print(''.join(friends_list))
+
+print(''.join(msg.split()))
+print(msg.replace(' ', ''))
